9559c02393e4205bce636564d16df9a67427836c
Doron-Breska <124248816+Doron-Breska@users.noreply.github.com>
2023-11-24 00:47:07 +0100

    about page without img

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,16 +14,20 @@ import {
 
 const Footer = () => {
   return (
-    <div id="footer" className="py-10">
-      <p className="text-center text-2xl  md:font-semibold">STAY IN TOUCH</p>
+    <div id="footer" className="py-14">
+      <p className="text-center text-2xl  md:font-semibold pb-7">
+        STAY IN TOUCH
+      </p>
       <div className="text-center">
         <p className="inline mx-3">MAKE AN ORDER</p>
-        <FontAwesomeIcon
-          icon={faEnvelope}
-          size="xl"
-          style={{ color: "#62554a" }}
-          className="mx-3"
-        />
+        <a href="mailto:madamepalomabakehouse@gmail.com?subject=Inquiry&body=Hello%20Madame%20Paloma%20Bakehouse,">
+          <FontAwesomeIcon
+            icon={faEnvelope}
+            size="xl"
+            style={{ color: "#62554a" }}
+            className="mx-3"
+          />
+        </a>
         <a href="tel:+4917661299624">
           <FontAwesomeIcon
             icon={faPhone}
@@ -33,25 +37,44 @@ const Footer = () => {
           />
         </a>
       </div>
-      <div className="icons text-center pt-5">
-        <FontAwesomeIcon
-          icon={faInstagram}
-          size="xl"
-          style={{ color: "#62554a" }}
-          className="mx-3"
-        />
-        <FontAwesomeIcon
-          icon={faTiktok}
-          size="xl"
-          style={{ color: "#62554a" }}
-          className="mx-3"
-        />
-        <FontAwesomeIcon
-          icon={faFacebookF}
-          size="xl"
-          style={{ color: "#62554a" }}
-          className="mx-3"
-        />
+      <div className="icons text-center pt-7">
+        <a
+          href="https://www.instagram.com/madame.paloma/?hl=en"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon
+            icon={faInstagram}
+            size="xl"
+            style={{ color: "#62554a" }}
+            className="mx-3"
+          />
+        </a>
+
+        <a
+          href="https://www.tiktok.com/@madame.paloma.bakehouse?lang=en"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon
+            icon={faTiktok}
+            size="xl"
+            style={{ color: "#62554a" }}
+            className="mx-3"
+          />
+        </a>
+        <a
+          href="https://www.facebook.com/p/Madame-Paloma-Bakehouse-100085219417779/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon
+            icon={faFacebookF}
+            size="xl"
+            style={{ color: "#62554a" }}
+            className="mx-3"
+          />
+        </a>
         <a
           href="https://www.google.com/maps/search/?api=1&query=Madame+Paloma+Bakehouse"
           target="_blank"
@@ -68,6 +91,5 @@ const Footer = () => {
     </div>
   );
 };
-//(98,85,74)
 
 export default Footer;
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,26 +1,32 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import { useEffect, useState } from "react";
-// import { json } from "stream/consumers";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
-  const [productsInfo, setProductsInfo] = useState([]);
-  useEffect(() => {
-    fetch("/api/products")
-      .then((response) => response.json())
-      .then((json) => setProductsInfo(json));
-  }),
-    [];
-
   return (
-    <div style={{ background: "green" }}>
-      <h1>products</h1>
-      {productsInfo.length > 0 &&
-        productsInfo.map((item: any) => {
-          return <div key={item._id}>{item.name}</div>;
-        })}
+    <div className="flex flex-row px-4 gap-14 about">
+      <div className="w-2/5">
+        <p className="text-2xl md:font-semibold tracking-wide mb-10">
+          MADAME PALOMA IS THE FIRST BUSINESS VENTURE OF EREZ PINTOK & HILLA
+          SASSON. EREZ, OUR CHEF, HAILS FROM ISRAEL&#39;S GALILEE SEA REGION,
+          BORN INTO A FAMILY OF CHEFS WITHIN A DIVERSE CULTURAL MIX OF EAST AND
+          WEST, JEWS AND ARABS. HIS CULINARY JOURNEY LED HIM TO PARIS IN 2014,
+          WHERE HE PURSUED HIS DREAM AT “LE CORDON BLEU”, ONE OF THE WORLD&#39;S
+          MOST PRESTIGIOUS CULINARY INSTITUTIONS. HILLA, ALSO FROM ISRAEL, HAS A
+          RICH BACKGROUND IN COMMUNICATION AND MEDIA. WITH A DECADE&#39;S
+          EXPERIENCE AS A JOURNALIST AND PR CONSULTANT, SHE JOINED EREZ ON HIS
+          PARISIAN ADVENTURE.
+        </p>
+        <p className="text-lg tracking-wide mb-10">
+          Madame Paloma opened its doors in September 2022. As a Bakehouse, we
+          specialize in Viennoiseries, offering an array of sweet and savory
+          premium pastries. Each creation is crafted from real French flour,
+          embodying a blend of tradition and innovation. Our pastries not only
+          tantalize the taste buds but also tell a story of culinary expertise
+          and passion.
+        </p>
+      </div>
+      <img
+        src="https://res.cloudinary.com/danq3q4qv/image/upload/v1699313211/imgs/u3mimw4p863pa3l8yhcl.jpg"
+        alt="pic of Hilla and Erez"
+        className="w-3/5 mb-10"
+      />
     </div>
   );
 }
