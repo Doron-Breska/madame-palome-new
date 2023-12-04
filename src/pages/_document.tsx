659a5749053458c981ac9f09ede0912bbb993b9a
Doron-Breska <124248816+Doron-Breska@users.noreply.github.com>
2023-12-04 18:25:17 +0100

    new icon and meta data

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>Madame Paloma Bakehouse</title>
+      <link rel="icon" type="image/png" href="icon2.png" />
+      <meta
+        name="Madame Paloma Bakehouse"
+        content="Discover the art of fine baking at Madame Paloma Bakehouse, a specialty bakery renowned for its exquisite brioche, croissants, and a variety of patisserie delights. Our artisanal approach combines traditional methods with innovative flavors, offering an unforgettable culinary experience in every bite."
+      />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
