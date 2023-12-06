d2ab752a8c7cd7159594fc113a7d76e5d35c7da3
Doron-Breska <124248816+Doron-Breska@users.noreply.github.com>
2023-12-06 18:45:50 +0100

    removed file type from the imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 // pages/index.js
-import connectMongoDB from "../../lib/mongoose.js";
-import Product from "../../models/Product.js";
+import connectMongoDB from "../../lib/mongoose";
+import Product from "../../models/Product";
 export default function Home({ productsInfo }: any) {
   return (
     <div className="M-D-S">
